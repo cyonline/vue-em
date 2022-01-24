e45e4a03db4e1b32db50f621deb83c4b1f75ee46
cyonline <chengyuonline@163.com>
2022-01-24 18:08:06 +0800

    路由bug修改

---
+++ src/router/modules/engineerManageRoutes.js
@@ -0,0 +1,36 @@
+const routes = [
+    {
+        path: '/',
+        redirect: 'engineerLookBoard/group',
+    },
+    {
+        path: 'engineerLookBoard',
+        name: 'engineerLookBoard',
+        component: () => import('@/views/lookBoard/index.vue'),
+        children: [
+            
+            {
+                path: 'group',
+                name: 'engineerLookBoard-group',
+                component: () => import('@/views/lookBoard/engineerLookBoard/group.vue'),
+            },
+            {
+                path: 'company',
+                name: 'engineerLookBoard-company',
+                component: () => import('@/views/lookBoard/engineerLookBoard/company.vue'),
+            }, {
+                path: 'project',
+                name: 'engineerLookBoard-project',
+                component: () => import('@/views/lookBoard/engineerLookBoard/project.vue'),
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirect: 'engineerLookBoard',
+    },
+
+
+]
+
+export default routes;
